86e5f077f8feae48751daf8ab310dc90e0c0a085
Matthias Bollwein <matthias.bollwein@tngtech.com>
2013-10-15 13:36:53 +0200

    completed tests

--- src/test/java/com/tngtech/configbuilder/ConfigBuilderTest.java
+++ src/test/java/com/tngtech/configbuilder/ConfigBuilderTest.java
@@ -1,20 +1,29 @@
 package com.tngtech.configbuilder;
 
 import com.google.common.collect.Lists;
+import com.tngtech.configbuilder.annotation.configuration.LoadingOrder;
 import com.tngtech.configbuilder.configuration.BuilderConfiguration;
 import com.tngtech.configbuilder.configuration.ErrorMessageSetup;
 import com.tngtech.configbuilder.testclasses.TestConfig;
 import com.tngtech.configbuilder.util.*;
 import com.tngtech.propertyloader.PropertyLoader;
+import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.lang.annotation.Annotation;
 import java.util.List;
+import java.util.Properties;
 
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -22,6 +31,8 @@ import static org.mockito.Mockito.when;
 public class ConfigBuilderTest {
 
     private ConfigBuilder<TestConfig> configBuilder;
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 
     @Mock
     private BuilderConfiguration builderConfiguration;
@@ -43,16 +54,34 @@ public class ConfigBuilderTest {
     private PropertyLoader propertyLoader;
     @Mock
     private Options commandLineOptions;
+    @Mock
+    private CommandLine commandLine;
+    @Mock
+    private Properties properties;
+    @Mock
+    private LoadingOrder loadingOrder;
 
     @Before
     public void setUp() throws Exception {
+
+        System.setOut(new PrintStream(outContent));
+
         when(propertyLoaderConfigurator.configurePropertyLoader(TestConfig.class)).thenReturn(propertyLoader);
+        when(commandLineHelper.getOptions(TestConfig.class)).thenReturn(commandLineOptions);
         configBuilder = new ConfigBuilder<>(TestConfig.class, builderConfiguration, propertyLoaderConfigurator, commandLineHelper, configValidator, fieldSetter, errorMessageSetup, constructionHelper);
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
     }
 
     @Test
     public void testWithCommandLineArgs() throws Exception {
-
+        when(commandLineHelper.getCommandLine(TestConfig.class,new String[]{})).thenReturn(commandLine);
+        configBuilder.withCommandLineArgs(new String[]{});
+        verify(commandLineHelper).getCommandLine(TestConfig.class, new String[]{});
+        verify(builderConfiguration).setCommandLine(commandLine);
     }
 
     @Test
@@ -64,16 +93,32 @@ public class ConfigBuilderTest {
 
     @Test
     public void testPrintCommandLineHelp() throws Exception {
-
+        configBuilder.printCommandLineHelp();
+        assertTrue(outContent.toString().contains("Command Line Options for class TestConfig"));
     }
 
     @Test
     public void testBuild() throws Exception {
+        when(propertyLoader.load()).thenReturn(properties);
+        configBuilder.build();
+        verify(propertyLoader).load();
+        verify(builderConfiguration).setProperties(properties);
+        verify(errorMessageSetup).initialize(null, propertyLoader);
 
+        verify(fieldSetter).setFields(Matchers.any(TestConfig.class),Matchers.any(BuilderConfiguration.class));
+        verify(configValidator).validate(Matchers.any(TestConfig.class));
     }
 
     @Test
     public void testMerge() throws Exception {
+        TestConfig testConfig = new TestConfig();
+        when(propertyLoader.load()).thenReturn(properties);
+        configBuilder.merge(testConfig);
+        verify(propertyLoader).load();
+        verify(builderConfiguration).setProperties(properties);
+        verify(errorMessageSetup).initialize(null,propertyLoader);
 
+        verify(fieldSetter).setEmptyFields(testConfig, builderConfiguration);
+        verify(configValidator).validate(testConfig);
     }
 }
