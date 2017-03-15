745b66454d38cfe8938513597bfba2c7b9b4e9a5
Manfred Hanke <Manfred.Hanke@tngtech.com>
2017-03-15 22:41:15 +0100

    avoid adding elements one by one to collection

--- src/main/java/com/tngtech/configbuilder/annotation/typetransformer/CharacterSeparatedStringToStringListTransformer.java
+++ src/main/java/com/tngtech/configbuilder/annotation/typetransformer/CharacterSeparatedStringToStringListTransformer.java
@@ -3,18 +3,11 @@ package com.tngtech.configbuilder.annotation.typetransformer;
 import com.google.common.collect.Lists;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 
 public class CharacterSeparatedStringToStringListTransformer extends TypeTransformer<String, ArrayList<String>> {
 
     @Override
     public ArrayList<String> transform(String argument) {
-
-        ArrayList<String> collection = Lists.newArrayList();
-        for (String value : argument.split((String)additionalOptions[0])) {
-            collection.add(value);
-        }
-        return collection;
+        return Lists.newArrayList(argument.split((String)additionalOptions[0]));
     }
 }
--- src/main/java/com/tngtech/configbuilder/annotation/typetransformer/CharacterSeparatedStringToStringSetTransformer.java
+++ src/main/java/com/tngtech/configbuilder/annotation/typetransformer/CharacterSeparatedStringToStringSetTransformer.java
@@ -3,17 +3,11 @@ package com.tngtech.configbuilder.annotation.typetransformer;
 import com.google.common.collect.Sets;
 
 import java.util.HashSet;
-import java.util.Set;
 
 public class CharacterSeparatedStringToStringSetTransformer extends TypeTransformer<String, HashSet<String>> {
 
     @Override
     public HashSet<String> transform(String argument) {
-
-        HashSet<String> collection = Sets.newHashSet();
-        for (String value : argument.split((String)additionalOptions[0])) {
-            collection.add(value);
-        }
-        return collection;
+        return Sets.newHashSet(argument.split((String)additionalOptions[0]));
     }
 }
