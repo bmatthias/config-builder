b219d71076a1f1da5bb33bf8175842bfe355e384
Manfred Hanke <Manfred.Hanke@tngtech.com>
2017-04-18 15:56:33 +0200

    (mostly typographic) improvements to README.md & LoadingOrder's JavaDoc

--- src/main/java/com/tngtech/configbuilder/annotation/configuration/LoadingOrder.java
+++ src/main/java/com/tngtech/configbuilder/annotation/configuration/LoadingOrder.java
@@ -2,15 +2,21 @@ package com.tngtech.configbuilder.annotation.configuration;
 
 import com.tngtech.configbuilder.annotation.valueextractor.CommandLineValue;
 import com.tngtech.configbuilder.annotation.valueextractor.DefaultValue;
+import com.tngtech.configbuilder.annotation.valueextractor.EnvironmentVariableValue;
+import com.tngtech.configbuilder.annotation.valueextractor.ImportedValue;
 import com.tngtech.configbuilder.annotation.valueextractor.PropertyValue;
+import com.tngtech.configbuilder.annotation.valueextractor.SystemPropertyValue;
+import com.tngtech.configbuilder.annotation.valueextractor.ValueExtractorAnnotation;
 
 import java.lang.annotation.*;
 
 /**
- * This annotation is used to specify the order in which the annotations CommandLineValue, PropertyValue and DefaultValue are processed.
+ * This annotation is used to specify the order in which the {@link ValueExtractorAnnotation} annotations
+ * {@link CommandLineValue}, {@link PropertyValue}, {@link EnvironmentVariableValue},
+ * {@link SystemPropertyValue}, {@link ImportedValue} and {@link DefaultValue} are processed.
  * It can specify the order for a certain field if placed on the field, or a global order if placed on the config class.
  * The annotations are processed top-down until a string value is found, i.e. the order is from the most important to the least important.
- * The order may only contain ValueExtractorAnnotations, i.e. CommandLineValue.class, PropertyValue.class and DefaultValue.class.<br>
+ * The {@code LoadingOrder} may only contain the aforementioned {@link ValueExtractorAnnotation} classes.<br>
  * <b>Usage:</b> <code>@LoadingOrder(value = {PropertyValue.class, CommandLineValue.class, DefaultValue.class})</code>
  */
 @Target({ElementType.TYPE, ElementType.FIELD})
