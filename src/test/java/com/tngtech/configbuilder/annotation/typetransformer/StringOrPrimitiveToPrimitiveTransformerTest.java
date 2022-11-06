12f0d1ebab8472cb717eaef7ab6077b213a21211
Manfred Hanke <Manfred.Hanke@tngtech.com>
2022-11-06 20:44:35 +0100

    simplify StringOrPrimitiveToPrimitiveTransformerTest
    Signed-off-by: Manfred Hanke <Manfred.Hanke@tngtech.com>

--- src/test/java/com/tngtech/configbuilder/annotation/typetransformer/StringOrPrimitiveToPrimitiveTransformerTest.java
+++ src/test/java/com/tngtech/configbuilder/annotation/typetransformer/StringOrPrimitiveToPrimitiveTransformerTest.java
@@ -5,6 +5,7 @@ import com.tngtech.configbuilder.exception.PrimitiveParsingException;
 import com.tngtech.configbuilder.util.ConfigBuilderFactory;
 import com.tngtech.configbuilder.util.FieldValueTransformer;
 import com.tngtech.configbuilder.util.GenericsAndCastingHelper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -15,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-public class StringToPrimitiveTransformerTest {
+public class StringOrPrimitiveToPrimitiveTransformerTest {
 
     private final StringOrPrimitiveToPrimitiveTransformer stringOrPrimitiveToPrimitiveTransformer = new StringOrPrimitiveToPrimitiveTransformer();
 
@@ -25,17 +26,15 @@ public class StringToPrimitiveTransformerTest {
     private ConfigBuilderFactory configBuilderFactory;
     @Mock
     private ErrorMessageSetup errorMessageSetup;
-    @Mock
-    private GenericsAndCastingHelper genericsAndCastingHelper;
+
+    @BeforeEach
+    void setupConfigBuilderFactoryMock() {
+        when(configBuilderFactory.getInstance(ErrorMessageSetup.class)).thenReturn(errorMessageSetup);
+        when(configBuilderFactory.getInstance(GenericsAndCastingHelper.class)).thenReturn(new GenericsAndCastingHelper());
+    }
 
     @Test
     public void testTransform() {
-        when(configBuilderFactory.getInstance(ErrorMessageSetup.class)).thenReturn(errorMessageSetup);
-        when(configBuilderFactory.getInstance(GenericsAndCastingHelper.class)).thenReturn(genericsAndCastingHelper);
-        when(genericsAndCastingHelper.castTypeToClass(boolean.class)).thenReturn((Class)boolean.class);
-        when(genericsAndCastingHelper.castTypeToClass(double.class)).thenReturn((Class)double.class);
-        when(genericsAndCastingHelper.castTypeToClass(int.class)).thenReturn((Class)int.class);
-
         stringOrPrimitiveToPrimitiveTransformer.initialize(fieldValueTransformer, configBuilderFactory);
 
         stringOrPrimitiveToPrimitiveTransformer.setTargetType(boolean.class);
@@ -62,11 +61,6 @@ public class StringToPrimitiveTransformerTest {
 
     @Test
     public void testThatSurroundingWhiteSpaceIsIgnored() {
-        when(configBuilderFactory.getInstance(ErrorMessageSetup.class)).thenReturn(errorMessageSetup);
-        when(configBuilderFactory.getInstance(GenericsAndCastingHelper.class)).thenReturn(genericsAndCastingHelper);
-        when(genericsAndCastingHelper.castTypeToClass(boolean.class)).thenReturn((Class)boolean.class);
-        when(genericsAndCastingHelper.castTypeToClass(int.class)).thenReturn((Class)int.class);
-
         stringOrPrimitiveToPrimitiveTransformer.initialize(fieldValueTransformer, configBuilderFactory);
 
         stringOrPrimitiveToPrimitiveTransformer.setTargetType(boolean.class);
@@ -82,10 +76,6 @@ public class StringToPrimitiveTransformerTest {
 
     @Test
     public void testTransformThrowsException() {
-        when(configBuilderFactory.getInstance(ErrorMessageSetup.class)).thenReturn(errorMessageSetup);
-        when(configBuilderFactory.getInstance(GenericsAndCastingHelper.class)).thenReturn(genericsAndCastingHelper);
-        when(genericsAndCastingHelper.castTypeToClass(int.class)).thenReturn((Class)int.class);
-
         stringOrPrimitiveToPrimitiveTransformer.initialize(fieldValueTransformer, configBuilderFactory);
         stringOrPrimitiveToPrimitiveTransformer.setTargetType(int.class);
         assertThrows(PrimitiveParsingException.class, () -> stringOrPrimitiveToPrimitiveTransformer.transform(1.0));
@@ -93,17 +83,10 @@ public class StringToPrimitiveTransformerTest {
 
     @Test
     public void testIsMatching() {
-        when(configBuilderFactory.getInstance(ErrorMessageSetup.class)).thenReturn(errorMessageSetup);
-        when(configBuilderFactory.getInstance(GenericsAndCastingHelper.class)).thenReturn(genericsAndCastingHelper);
-        when(genericsAndCastingHelper.isPrimitiveOrWrapper(int.class)).thenReturn(true);
-        when(genericsAndCastingHelper.isPrimitiveOrWrapper(Integer.class)).thenReturn(true);
-        when(genericsAndCastingHelper.isPrimitiveOrWrapper(Object.class)).thenReturn(false);
-
         stringOrPrimitiveToPrimitiveTransformer.initialize(fieldValueTransformer, configBuilderFactory);
 
         assertThat(stringOrPrimitiveToPrimitiveTransformer.isMatching(int.class, Integer.class)).isTrue();
         assertThat(stringOrPrimitiveToPrimitiveTransformer.isMatching(String.class, int.class)).isTrue();
         assertThat(stringOrPrimitiveToPrimitiveTransformer.isMatching(int.class, Object.class)).isFalse();
     }
-
 }
